761939d12c3130d2ae22e45e8721c97cc49722a5
yuhwanwoo <6514687@naver.com>
2021-01-24 11:57:24 +0900

    story

--- learning/App.js
+++ learning/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View,Button, Image} from 'react-native';
+import { StyleSheet, Text, View,Button, Image, ScrollView, } from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { IconButton, Colors } from 'react-native-paper';
 
-import {Header,Right,Left, Container, Thumbnail} from 'native-base'
+import {Header,Right,Left, Container, Thumbnail, Content, Card, CardItem, Body, Icon, } from 'native-base'
 
 function SettingsScreen({ route, navigation }) {
   const { user } = route.params;
@@ -24,36 +24,94 @@ function SettingsScreen({ route, navigation }) {
 
 function ProfileScreen({ navigation }) {
   return (
-    <Container >
+    <Container>
       <Header style={styles.test_nav}>
         <Left>
-          <Image style={styles.logo} source={{uri:'./learn.png'}}/>
-          <Thumbnail square style={styles.logo} source={{uri:'./learn.png'}}/>
+          
+          <Thumbnail square style={styles.logo} source={require('./image/learn.png')}/>
         </Left>
         <Right>
           <IconButton icon="menu" onPress={() => navigation.dispatch(DrawerActions.openDrawer())}></IconButton>
         </Right>
       </Header>
+      
+      <Content>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail style={{height:30, width:30}} source={require('./image/pro.jpg')} />
+                <Body>
+                  <Text>HwanwooYu</Text>
+                  <Text note>c++ init</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={require('./image/card.jpg')} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent title="추천 수">
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent title="평가 수">
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+          <Text>
+          </Text>
+          <ScrollView
+  horizontal={true}
+  showsHorizontalScrollIndicator={false}
+  contentContainerStyle={{
+  alignItems: 'center',
+  paddingStart: 5,
+  paddingEnd: 5
+}}>
+  <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://storage.pixteller.com/designs/designs-images/2016-11-19/02/thumbs/img_page_1_58305b35ebf5e.png' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl26_3P-JK93cCsNUbfiLf2frvPpOrPz-YBg&usqp=CAU' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzjIwk8J6HySnA4Lbq4GgKv_og10cX0fBVbg&usqp=CAU' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://storage.pixteller.com/designs/designs-images/2016-11-19/02/thumbs/img_page_1_58305b35ebf5e.png' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl26_3P-JK93cCsNUbfiLf2frvPpOrPz-YBg&usqp=CAU' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzjIwk8J6HySnA4Lbq4GgKv_og10cX0fBVbg&usqp=CAU' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://storage.pixteller.com/designs/designs-images/2016-11-19/02/thumbs/img_page_1_58305b35ebf5e.png' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl26_3P-JK93cCsNUbfiLf2frvPpOrPz-YBg&usqp=CAU' }} />
+    <Thumbnail 
+    style={{ marginHorizontal: 5, borderColor: 'pink', borderWidth: 2 }}
+    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzjIwk8J6HySnA4Lbq4GgKv_og10cX0fBVbg&usqp=CAU' }} />
+    
+    </ScrollView>
+        </Content>
 
-      <View style={{alignItems: 'center', alignContent:'center',}}>
-      <Thumbnail square style={styles.logo} source={{uri:'learn.png'}}/>
-      <Image style={styles.logo} source={{uri:'./learn.png'}}/>
-      
-      <Text>Profile Screen</Text>
-      <View>
-      <Text>test</Text>
-    </View>
-    </View>
-    <View style={styles.container}>
-      
-      <Image
-        style={styles.logo}
-        source={require('./image/learn.png')}
-      />
-      </View>
+        
       </Container>
     
-    
+
   );
 }
 
@@ -117,7 +175,7 @@ const styles = StyleSheet.create({
   },
   logo:{
     
-    width:80,
-    height:80,
+    width:40,
+    height:40,
   },
 });
